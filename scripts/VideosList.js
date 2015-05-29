1510b431e0a4b96467f5a41d63729c510f471340
ButenkoT <tanya@butenko.me>
2015-05-29 13:41:58 +1000

    base video list

--- scripts/VideosList.js
+++ scripts/VideosList.js
@@ -3,11 +3,54 @@ import React from 'react'
 export default class VideosList extends React.Component {
   render(){
     return(
-      <ol>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ol>
+      <div className="videosList">
+        <div className="videosListHeader">
+          <h4>Latest Videos</h4>
+
+          <div className="sorting">Sort by
+            <select name="sortBy">
+              <option value="mRecent">Most recent</option>
+              <option value="mPopular">Most popular this month</option>
+            </select>
+          </div>
+        </div>
+
+        <ol>
+          <li>
+            <div>
+              <section className="votes">V</section>
+              <img src="" alt=""/>
+              <a href="">This is a regular video title</a>
+              <div className="videoInfo">
+                <p>David Lynch</p>
+                <p>1month ago * 2 views</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <section className="votes">V</section>
+              <img src="" alt=""/>
+              <a href="">This is a regular video title</a>
+              <div className="videoInfo">
+                <p>David Lynch</p>
+                <p>1month ago * 2 views</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <section className="votes">V</section>
+              <img src="" alt=""/>
+              <a href="">This is a regular video title</a>
+              <div className="videoInfo">
+                <p>David Lynch</p>
+                <p>1month ago * 2 views</p>
+              </div>
+            </div>
+          </li>
+        </ol>
+      </div>
     );
   }
 }
