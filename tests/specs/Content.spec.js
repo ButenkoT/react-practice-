f95a5977bcb80690712c6ddf12536261017c3f4b
ButenkoT <tanya@butenko.me>
2015-11-03 15:15:06 +1100

    uninstall jasmine and karma

--- tests/specs/Content.spec.js
+++ tests/specs/Content.spec.js
@@ -1,13 +1,13 @@
-const Content = require('../../scripts/Content');
-
-
-describe('Content', () => {
-  it('Should render greetings', () => {
-    const content = TestUtils.renderIntoDocument(<Content />);
-
-    const h1 = TestUtils.findRenderedDOMComponentWithTag(content, 'h1');
-    expect(TestUtils.isDOMComponent(h1)).toBeTruthy();
-    expect(h1.getDOMNode().textContent.match('video')).toBeTruthy();
-  });
-
-});
+//const Content = require('../../scripts/Content');
+//
+//
+//describe('Content', () => {
+//  it('Should render greetings', () => {
+//    const content = TestUtils.renderIntoDocument(<Content />);
+//
+//    const h1 = TestUtils.findRenderedDOMComponentWithTag(content, 'h1');
+//    expect(TestUtils.isDOMComponent(h1)).toBeTruthy();
+//    expect(h1.getDOMNode().textContent.match('video')).toBeTruthy();
+//  });
+//
+//});
