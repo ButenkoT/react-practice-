cb660ca27c79adbf1ebcbf72f98c20840ee9802d
ButenkoT <tanya@butenko.me>
2015-11-03 14:59:52 +1100

    e2e nightwatch tests setup

---
+++ e2e_tests/Header.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'Test for header appears on the screen': function (browser) {
+    browser
+      .url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('#root', 1000)
+      .waitForElementVisible('header', 1000)
+      .assert.containsText('header', '15 Minutes Video Competition')
+      .end();
+  }
+};
